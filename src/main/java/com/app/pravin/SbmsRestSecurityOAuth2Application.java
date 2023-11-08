4e197639729b2a204450425588c2d89e59148e85
pravinjenkins <pravinnikure300@gmail.com>
2023-11-08 19:12:23 +0530

    Initial Commit

---
+++ src/main/java/com/app/pravin/SbmsRestSecurityOAuth2Application.java
@@ -0,0 +1,13 @@
+package com.app.pravin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsRestSecurityOAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsRestSecurityOAuth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/pravin/SbmsRestSecurityOAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.pravin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmsRestSecurityOAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
