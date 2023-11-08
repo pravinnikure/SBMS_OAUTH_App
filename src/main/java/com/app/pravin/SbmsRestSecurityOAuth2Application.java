5f3c12f332e7ff8320a3e6c711566fcacb7daaec
pravinjenkins <pravinnikure300@gmail.com>
2023-11-08 19:27:30 +0530

    Adding desc about app in main

--- src/main/java/com/app/pravin/SbmsRestSecurityOAuth2Application.java
+++ src/main/java/com/app/pravin/SbmsRestSecurityOAuth2Application.java
@@ -8,6 +8,8 @@ public class SbmsRestSecurityOAuth2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbmsRestSecurityOAuth2Application.class, args);
+		
+		System.out.println("Spring boot application for Spring OAuth2.X ");
 	}
 
 }
